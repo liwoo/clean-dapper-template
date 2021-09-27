9acd362a8634ade28877c46ea4a438199d09a852
Malumbo Mkandawire <mmalumbo@gmail.com>
2021-09-27 07:38:38 +0200

    Updated team

--- src/Domain/Entities/Team.cs
+++ src/Domain/Entities/Team.cs
@@ -1,4 +1,3 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing;
@@ -9,12 +8,23 @@ namespace Domain.Entities
     [Table("teams")]
     public record Team
     {
+        [Key]
+        [Column("id")]
         public int Id { get; init; }
+
+        [Column("name")]
         public string Name { get; init; }
+
+        [Column("position")]
         public int Position { get; init; }
+
+        [Column("stadium")]
+        public string Stadium { get; init; }
+
         [Column("home_kit_color")]
         public KnownColor HomeKitColor { get; init; }
-        public string Stadium { get; init; }
+
+        [Column("city")]
         public Cities City { get; init; }
     }
 }
